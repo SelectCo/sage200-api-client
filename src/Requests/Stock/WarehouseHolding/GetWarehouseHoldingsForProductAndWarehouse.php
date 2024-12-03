0a8d4567cf9a890bb0001f82afec56c69f4b2b22
adrian.bartrum <adrian@Selectionclothing.com>
2024-12-03 17:17:46 +0000

    Add WarehouseHolding request

---
+++ src/Requests/Stock/WarehouseHolding/GetWarehouseHoldingsForProductAndWarehouse.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Selectco\SageApi\Requests\Stock\WarehouseHolding;
+
+use JsonException;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Selectco\SageApi\DataObjects\Stock\WarehouseHolding;
+
+class GetWarehouseHoldingsForProductAndWarehouse extends Request
+{
+    protected Method $method = Method::GET;
+    private string $endPoint;
+
+    /**
+     * @param int $productId Unique id of the product.
+     * @param int $warehouseId Unique id of the warehouse.
+     */
+    public function __construct(protected int $productId, protected int $warehouseId)
+    {
+        $this->endPoint = "/products/{$this->productId}/warehouses/{$this->warehouseId}/warehouse_holdings";
+    }
+
+    /**
+     * Define the endpoint for the request.
+     */
+    public function resolveEndpoint(): string
+    {
+        return $this->endPoint;
+    }
+
+    /**
+     * @param string $queryParameters
+     * @return void
+     */
+    public function setQueryParameters(string $queryParameters): void
+    {
+        $this->endPoint .= $queryParameters;
+    }
+
+    /**
+     * @param Response $response
+     * @return WarehouseHolding[]
+     * @throws JsonException
+     */
+    public function createDtoFromResponse(Response $response): array
+    {
+        $returnArray = [];
+        foreach ($response->json() as $data) {
+            $returnArray[] = new WarehouseHolding(...$data);
+        }
+        return $returnArray;
+    }
+}
