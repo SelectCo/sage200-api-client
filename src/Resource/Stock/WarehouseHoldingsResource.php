e6c456730844a6aec252dfa1a84525c2c5cb798f
adrian.bartrum <adrian@Selectionclothing.com>
2024-12-03 18:09:37 +0000

    Amend WarehouseHoldingsResource.php

--- src/Resource/Stock/WarehouseHoldingsResource.php
+++ src/Resource/Stock/WarehouseHoldingsResource.php
@@ -3,16 +3,21 @@ declare(strict_types=1);
 
 namespace Selectco\SageApi\Resource\Stock;
 
+use Saloon\Http\Response;
 use Selectco\SageApi\DataObjects\Stock\WarehouseHolding;
 use Saloon\Exceptions\Request\FatalRequestException;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Http\Connector;
+use Selectco\SageApi\Exception\DataValidationException;
+use Selectco\SageApi\Requests\Stock\WarehouseHolding\DelWarehouseHolding;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHolding;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldings;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldingsForProduct;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldingsForProductAndWarehouse;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldingsForWarehouse;
 use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldingsForWarehouseAndProduct;
+use Selectco\SageApi\Requests\Stock\WarehouseHolding\PostWarehouseHoldings;
+use Selectco\SageApi\Requests\Stock\WarehouseHolding\PutWarehouseHolding;
 
 class WarehouseHoldingsResource
 {
@@ -21,6 +26,40 @@ class WarehouseHoldingsResource
      */
     public function __construct(protected Connector $connector)
     {
+    }
+
+    /**
+     * @param string|null $queryParameters
+     * @return WarehouseHolding[]
+     * @throws FatalRequestException
+     * @throws RequestException
+     */
+    public function getWarehouseHoldings(string|null $queryParameters = ''): array
+    {
+        $warehouseHolding = new GetWarehouseHoldings();
+        $warehouseHolding->setQueryParameters($queryParameters);
+        return $this->connector->send($warehouseHolding)->dto();
+    }
+
+    /**
+     * @param array $data
+     * @return WarehouseHolding
+     * @throws DataValidationException
+     * @throws FatalRequestException
+     * @throws RequestException
+     */
+    public function postWarehouseHoldings(array $data): WarehouseHolding
+    {
+        $warehouse = new PostWarehouseHoldings($data);
+        $errors = $warehouse->verifyData();
+
+        if (count($errors) > 0)
+        {
+            $errorsString = (string) $errors;
+            throw new DataValidationException($errorsString);
+        }
+
+        return $this->connector->send($warehouse)->dto();
     }
 
     /**
@@ -38,16 +77,36 @@ class WarehouseHoldingsResource
     }
 
     /**
-     * @param string|null $queryParameters
-     * @return WarehouseHolding[]
+     * @param int $id
+     * @param array $data
+     * @return WarehouseHolding
+     * @throws DataValidationException
      * @throws FatalRequestException
      * @throws RequestException
      */
-    public function getWarehouseHoldings(string|null $queryParameters = ''): array
+    public function putWarehouseHolding(int $id, array $data): WarehouseHolding
     {
-        $warehouseHolding = new GetWarehouseHoldings();
-        $warehouseHolding->setQueryParameters($queryParameters);
-        return $this->connector->send($warehouseHolding)->dto();
+        $warehouse = new PutWarehouseHolding($id, $data);
+        $errors = $warehouse->verifyData();
+
+        if (count($errors) > 0)
+        {
+            $errorsString = (string) $errors;
+            throw new DataValidationException($errorsString);
+        }
+
+        return $this->connector->send($warehouse)->dto();
+    }
+
+    /**
+     * @param int $warehouseId
+     * @return Response
+     * @throws FatalRequestException
+     * @throws RequestException
+     */
+    public function delWarehouseHolding(int $warehouseId): Response
+    {
+        return $this->connector->send(new DelWarehouseHolding($warehouseId));
     }
 
     /**
