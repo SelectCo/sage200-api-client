e08de7eacc264bc6b4ccd69eea4915975cd17a2c
adrian.bartrum <adrian@Selectionclothing.com>
2024-12-03 17:13:07 +0000

    Add Customer DataObject

---
+++ src/DataObjects/Sales/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Selectco\SageApi\DataObjects\Sales;
+
+use Selectco\SageDTO\Sales\CustomerDTO;
+
+
+class Customer extends CustomerDTO
+{
+}
