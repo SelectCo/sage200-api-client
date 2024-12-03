c14618b8f7a1331fb233edc9de590bfd04e6044c
adrian.bartrum <adrian@Selectionclothing.com>
2024-12-03 17:11:51 +0000

    Add WarehouseHolding.php DataObject

---
+++ src/DataObjects/Stock/WarehouseHolding.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Selectco\SageApi\DataObjects\Stock;
+
+use Selectco\SageDTO\Stock\WarehouseHoldingDTO;
+
+class WarehouseHolding extends WarehouseHoldingDTO
+{
+
+}
