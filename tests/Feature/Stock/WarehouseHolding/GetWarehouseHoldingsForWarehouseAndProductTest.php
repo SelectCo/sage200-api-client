ebbeebcc42d605f9fa32a77a53b40178b0b46ee2
adrian.bartrum <adrian@Selectionclothing.com>
2025-01-15 17:32:49 +0000

    Add Stock/WarehouseHolding GET tests

---
+++ tests/Feature/Stock/WarehouseHolding/GetWarehouseHoldingsForWarehouseAndProductTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+//Load default test cases
+require_once getDefaultMultipleObjectTestPath();
+
+use Selectco\SageApi\DataObjects\Stock\WarehouseHolding;
+use Selectco\SageApi\Requests\General\Sites\GetSites;
+use Selectco\SageApi\Requests\Stock\WarehouseHolding\GetWarehouseHoldingsForWarehouseAndProduct;
+use Selectco\SageApi\Sage200Connector;
+
+test('connector', function () {
+    $connector = new Sage200Connector(
+        developerSubscriptionKey: getDeveloperSubscriptionKey(),
+        bearerToken: getBearerToken()
+    );
+    $response = $connector->send(new GetSites());
+
+    expect($response->status())->toEqual(200);
+});
+
+/**
+ * TEST REQUEST WITH ID AND MULTIPLE RESULTS
+ */
+test('id', function() {
+    return 1;
+})->depends('connector');
+
+test('request', function () {
+    return GetWarehouseHoldingsForWarehouseAndProduct::class;
+});
+
+test('object', function () {
+    return WarehouseHolding::class;
+});
