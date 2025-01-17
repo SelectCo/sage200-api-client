4c0f763b5d848da51c11b1dc1c7601e1cecd0e02
adrian.bartrum <adrian@Selectionclothing.com>
2025-01-17 17:58:27 +0000

    Modify Resources

--- src/Resource/Sales.php
+++ src/Resource/Sales.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 namespace Selectco\SageApi\Resource;
 
 use Saloon\Http\Connector;
+use Selectco\SageApi\Resource\Sales\CustomersResource;
 use Selectco\SageApi\Resource\Sales\SalesPostedTransactionsResource;
 
 class Sales
 {
     public function __construct(protected Connector $connector)
     {
+    }
+
+    public function customers(): CustomersResource
+    {
+        return new CustomersResource($this->connector);
     }
 
     public function salesPostedTransactions(): SalesPostedTransactionsResource
